b3f76eee0a44782638067f22f437bef3d8a8ac41
Jim Holmes <Jim@GuidepostSystems.com>
2018-04-30 04:31:29 -0700

    Initial

---
+++ Settings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaneWebDriver_CSharp
+{
+    public static class Settings
+    {
+        public static string SITE_URL = "http://demosite.com";
+        public static string CONTACT_URI = "http://demosite.com/api/contact";
+    }
+}
