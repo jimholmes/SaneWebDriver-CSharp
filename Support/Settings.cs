1a43869d84c6e251c35807ccc1d76efc11f5e9a2
Jim Holmes <Jim@GuidepostSystems.com>
2018-05-09 07:51:21 -0700

    Description of Settings' purpose.

--- Support/Settings.cs
+++ Support/Settings.cs
@@ -9,7 +9,20 @@ namespace SaneWebDriver_CSharp.Support
 {
     public static class Settings
     {
-        public static string SITE_URL = "http://demosite.com";
-        public static string CONTACT_URI = "http://demosite.com/api/contact";
+        /*
+         * These settings should *NOT* be hard-coded in a real test suite.
+         * Instead, they should read from envrionment variables,
+         * a customized text file, or whatever other mechanism you might 
+         * use in your build system.
+         * 
+         * For example, Jenkins builds might set environment properties to read.
+         * Travis CI might drop an environment-specific text file. VSTS might
+         * read from an App.config file. Etc.
+         * 
+         * This is merely showing one place to abstract all settings so
+         * your tests don't know about them.
+         */
+        public static string SITE_URL = "http://jhdemos.azurewebsites.net/";
+        public static string CONTACT_URI = "http://jhdemos.azurewebsites.net/api/contact";
     }
 }
